943c09beb58681dba0f0f0da95cb98701be0beb1
gaston <gastonledes9@gmail.com>
2023-03-09 19:45:51 -0300

    Primer version

---
+++ Club/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from Club.models import *
+from django.http import HttpResponse
+from Club.forms import *
+
+# Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def jugadores(request):
+    if request.method == 'POST':
+        miformulario = Jugadoresformulario(request.POST)
+
+        print(miformulario)
+        
+        if miformulario.is_valid:
+            
+            informacion= miformulario.cleaned_data
+
+            jugador = Jugadores(nombre=informacion['nombre'], apellido=informacion['apellido'], categoria= informacion['categoria'], numerocarnet= informacion['numerocarnet'])
+            jugador.save()
+            print('Jugador registrado.') 
+            return render(request, 'Inicio.html')
+    else:
+        miformulario= Jugadoresformulario
+
+    return render(request, 'jugadores.html', {'miformulario': miformulario})
+
+def buscar(request):
+    if request.GET['apellido']:
+        apellido= request.GET['apellido']
+        nombre= Jugadores.objects.filter(apellido__icontains=apellido)
+        categoria= Jugadores.objects.filter(apellido__icontains=apellido)
+        numerocarnet =Jugadores.objects.filter(apellido__icontains=apellido)
+        
+        return render(request,'Inicio.html', {"nombre": nombre, "apellido": apellido, "categoria": categoria, "numerocarnet": numerocarnet})
+    else:
+        respuesta='No enviaste datos.'
+    return render(request, 'Inicio.html', {"respuesta": respuesta})
+
+
+
+
+def entrenadores(request):
+    if request.method == 'POST':
+        miformulario = Entrenadoresformulario(request.POST)
+
+        print(miformulario)
+        
+        if miformulario.is_valid:
+            
+            informacion= miformulario.cleaned_data
+
+            jugador = Entrenadores(nombre=informacion['nombre'], apellido=informacion['apellido'], categoria_cargo= informacion['Categoria a cargo'])
+            jugador.save()
+            print('Entrenador registrado.') 
+            return render(request, 'Inicio.html')
+    else:
+       miformulario= Entrenadoresformulario
+        
+    return render(request, 'entrenadores.html', {'miformulario': miformulario})
+
+
+def comisiondirectiva(request):
+    if request.method == 'POST':
+        miformulario = Comisionformulario(request.POST)
+
+        print(miformulario)
+        
+        if miformulario.is_valid:
+            
+            informacion= miformulario.cleaned_data
+
+            jugador = Comisiondirectiva(nombre=informacion['nombre'], 
+                                        apellido=informacion['apellido'], 
+                                        email=informacion['email'], 
+                                        telefono= informacion['telefono'], 
+                                        cargo=informacion['cargo'])
+            jugador.save()
+            print('Directivo registrado.') 
+            return render(request, 'Inicio.html')
+    else:
+        miformulario= Comisionformulario
+
+    return render(request, 'comisiondirectiva.html', {'miformulario': miformulario})
